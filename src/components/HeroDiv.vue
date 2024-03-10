c4b06dbecbd30954458c7b2313fadac8a5a735f5
dianaveselkina <vess_di@mail.ru>
2024-03-10 23:36:57 +0300

    add btn

---
+++ src/components/HeroDiv.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hero__container"></div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style></style>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="btn"><slot></slot></button>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style>
+.btn {
+  width: 400px;
+  height: 70px;
+  cursor: pointer;
+  background-color: #ea4c14;
+  font-size: 18px;
+  color: fff;
+}
+</style>
