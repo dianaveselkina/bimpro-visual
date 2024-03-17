7d2d9d02ee5416957963943622af8ee8f7e11068
dianaveselkina <vess_di@mail.ru>
2024-03-17 23:14:33 +0300

    add switch__block

--- src/components/MediaBlock.vue
+++ src/components/MediaBlock.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="media__container">
-    <h2 class="services__title">СМИ о нас</h2>
+    <div class="title">
+      <h2 class="services__title">СМИ о нас</h2>
+      <div class="switch">
+        <div class="switch__block">
+          <img class="arrow__img" src="/img/arrow_icon1.png" alt="arrow" />
+        </div>
+        <div class="switch__block">
+          <img class="arrow__img" src="/img/arrow_icon2.png" alt="arrow" />
+        </div>
+      </div>
+    </div>
     <div class="medias__cards">
       <MediaCard
         imageUrl="/img/media_img1.png"
@@ -37,10 +47,25 @@ export default {
   background-color: #2e2e31;
   color: white;
 }
+.title {
+  padding: 27px 0 80px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.switch {
+  display: flex;
+  flex-direction: row;
+}
+.switch__block {
+  cursor: pointer;
+  width: 50px;
+  height: 50px;
+  border: 1px solid #9da0a3;
+}
 .services__title {
   text-transform: uppercase;
   font-size: 50px;
-  padding: 27px 0 80px;
 }
 .medias__cards {
   display: flex;
