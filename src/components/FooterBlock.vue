c7a0b3f6d058bd5c1eed968eefe0d7ac1381e105
dianaveselkina <vess_di@mail.ru>
2024-03-18 18:52:34 +0300

    add directions block

--- src/components/FooterBlock.vue
+++ src/components/FooterBlock.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="footer">
-    <div class="footer__container">footer</div>
+    <div class="footer__container">
+      <div class="footer__top">
+        <div class="footer__logo">
+          <img class="logo__img" src="/img/logo__footer.png" alt="logo" />
+        </div>
+        <div class="footer__link">
+          <ul class="footer__link-item">
+            <li>
+              <a href="#">Услуги</a>
+            </li>
+            <li>
+              <a href="#">Преимущества</a>
+            </li>
+            <li>
+              <a href="#">Проекты</a>
+            </li>
+            <li>
+              <a href="#">Команда</a>
+            </li>
+            <li>
+              <a href="#">Гарантии</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="directions__block">
+        <ul class="footer__link-item">
+          <li>
+            <p>Другие наши направления:</p>
+          </li>
+          <li>
+            <a href="#">Проектирование</a>
+          </li>
+          <li>
+            <a href="#">Дизайн</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,12 +56,23 @@ ul {
   list-style: none;
 }
 .footer__container {
+  padding: 15px 277px 25px 277px;
+  background-color: #070707;
+  color: white;
+}
+.footer__top {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  padding: 15px 277px 25px 277px;
-  background-color: #070707;
-  color: white;
+}
+.logo__img {
+  width: 186px;
+  height: 47px;
+}
+.footer__link-item {
+  display: flex;
+  flex-direction: row;
+  gap: 35px;
 }
 </style>
