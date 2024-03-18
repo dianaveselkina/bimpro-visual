4d063f49c33d9e504daafa9f41b1972bcc1a8975
dianaveselkina <vess_di@mail.ru>
2024-03-18 19:02:12 +0300

    add footer map

--- src/components/FooterBlock.vue
+++ src/components/FooterBlock.vue
@@ -38,6 +38,21 @@
           </li>
         </ul>
       </div>
+      <div class="footer__contacts">
+        <div class="footer__map">
+          <p>г. Санкт-Петербург, Шпалерная ул. д. 51</p>
+          <div class="email__block">
+            <p>+7 812 330 10 40</p>
+            <p>project@bimpro.ru</p>
+          </div>
+          <div class="schedule__block">
+            <p>ПН-ПТ 9:00 — 18:00</p>
+            <p>СБ, ВС — Выходные</p>
+          </div>
+          <img class="map__img" src="/img/map.png" alt="map" />
+        </div>
+        <div class="telephone__block"></div>
+      </div>
     </div>
   </div>
 </template>
